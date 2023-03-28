4893093a55c72473432c18235bc1e2562651f2fa
Jane Kostevich <yana.kostevich18@gmail.com>
2023-03-28 21:31:11 +0300

    menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <video
+      class="bg-video"
+      loop=""
+      autoplay="true"
+      muted
+      src="../assets/video.mp4"
+      type="video/mp4"
+    ></video>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/layouts/Header.vue";
 
+console.log(Header);
 export default {
-  name: "HomeView",
   components: {
-    HelloWorld,
+    Header,
   },
 };
 </script>
+
+<style lang="scss">
+.bg-video {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  object-fit: cover;
+  filter: brightness(50%) blur(2px);
+}
+</style>
